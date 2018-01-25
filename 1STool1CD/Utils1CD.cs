d0494164622b533023a185d1db44418048a5b014
fishca <sergrud@mail.ru>
2018-01-25 16:46:06 +0300

    Добавлены константы

---
+++ 1STool1CD/Utils1CD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1STool1CD
+{
+    public class Utils1CD
+    {
+        /// <summary>
+        /// Версии формата базы 1С
+        /// </summary>
+        public enum db_ver
+        {
+            ver8_0_3_0  = 1,
+        	ver8_0_5_0  = 2,
+	        ver8_1_0_0  = 3,
+	        ver8_2_0_0  = 4,
+	        ver8_2_14_0 = 5,
+	        ver8_3_8_0  = 6
+        }
+
+    }
+}
