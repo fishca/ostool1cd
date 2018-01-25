a766b3e9a70bbf50e78d052ac25720bc876929b8
fishca <sergrud@mail.ru>
2018-01-25 23:19:10 +0300

    TableFileStream заготовка

---
+++ 1STool1CD/TableFileStream.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _1STool1CD
+{
+    /// <summary>
+    /// Класс таблицы контейнера файлов (CONFIG, CONFIGSAVE, PARAMS, FILES, CONFICAS, CONFICASSAVE)
+    /// </summary>
+    class TableFileStream : FileStream
+    {
+        #region public
+
+        TableFileStream(table_file tf) : base(" ", FileMode.Open) { }
+
+        public virtual Int64 Read(byte[] Buffer, Int64 Count) { return 0; }
+	    public override Int32 Read(byte[] Buffer, Int32 Offset, Int32 Count) { return 0; }
+        public virtual Int64 Write(byte[] Buffer, Int64 Count)  { throw new Exception("Write read-only stream"); }
+        public override void Write(byte[] Buffer, Int32 Offset, Int32 Count) { throw new Exception("Write read-only stream"); }
+        public virtual Int32 Seek(Int32 Offset, UInt16 Origin) { return 0; }
+        public override Int64 Seek(Int64 Offset, SeekOrigin Origin) { return 0; }
+
+        #endregion
+
+        #region private
+        private Int64 curoffset;
+        private table_file tablefile;
+        private Stream streams;
+        #endregion
+
+    }
+}
