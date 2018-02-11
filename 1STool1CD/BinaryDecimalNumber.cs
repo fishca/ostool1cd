b5d446520d2c81b23e1f5bf3ebadbcf1a7be66c8
fishca <sergrud@mail.ru>
2018-02-11 19:21:40 +0300

    Очередная порция доработки

--- 1STool1CD/BinaryDecimalNumber.cs
+++ 1STool1CD/BinaryDecimalNumber.cs
@@ -6,19 +6,258 @@ using System.Threading.Tasks;
 
 namespace _1STool1CD
 {
+    public class BinaryDecimalBuilder
+    {
+        public List<int> data;
+
+        public BinaryDecimalBuilder(List<int> data) => this.data = data;
+
+        public void push1(byte lo)
+        {
+
+            //data.push_back(lo);
+            data.Add(lo);
+        }
+
+        public void push2(byte hilo)
+        {
+            push1((byte)(hilo >> 4));
+            push1((byte)(hilo & 0x0f));
+        }
+
+    }
+
     public class BinaryDecimalNumber
     {
-        public BinaryDecimalNumber() { }
-        public BinaryDecimalNumber(byte[] raw_data, int length, int precision, bool has_sign_flag) { }
-        public BinaryDecimalNumber(String presentation, bool has_sign = false, int length = 0, int precision = 0) { }
+        public BinaryDecimalNumber()
+        {
+        }
 
-        public void write_to(byte[] raw_data) { }
+        public BinaryDecimalNumber(byte raw_data, int length, int precision, bool has_sign_flag)
+        {
+            this.has_sing_flag = has_sign_flag;
+            this.precision = precision;
+            this.sign = 1;
 
-        public virtual String get_presentation() { return " "; }
-        public String get_part(int startIndex, int count) { return " "; }
+            this.data.Clear();
 
-        public List<int> get_int() { return null; }
-        public List<int> get_frac() { return null; }
+            BinaryDecimalBuilder builder = new BinaryDecimalBuilder(data);
+
+            byte byte_data = raw_data;
+            byte first_byte = byte_data;
+
+            if (has_sign_flag)
+            {
+                if (first_byte >> 4 != 0)
+                {
+                    sign = 1;
+                }
+                else
+                {
+                    sign = -1;
+                }
+                builder.push1((byte)(first_byte & 0x0f));
+            }
+            else
+            {
+                builder.push2(first_byte);
+            }
+            ++byte_data;
+            int i = 1;
+            while (i < length)
+            {
+                if (i + 1 < length)
+                {
+                    builder.push2(byte_data);
+                    i += 2;
+                }
+                else
+                {
+                    builder.push1((byte)(byte_data >> 4));
+                    i++;
+                }
+                byte_data++;
+            }
+
+
+        }
+
+        public BinaryDecimalNumber(String presentation, bool has_sign = false, int length = 0, int precision = 0)
+        {
+            this.has_sing_flag = has_sign;
+            this.precision = precision;
+            this.sign = 1;
+            int INT_PART = 0;
+            int FRAC_PART = 1;
+
+            List<int>[] parts = new List<int>[2];
+           
+            int part_no = 0;
+
+            foreach (Char c in presentation)
+            {
+                if (c == '.')
+                {
+                    part_no++;
+                }
+                else
+                {
+                    parts[part_no].Add(c - '0');
+                }
+            }
+            if (precision != 0)
+            {
+                for (int i = 0; i < precision; i++)
+                    parts[FRAC_PART].Add(0);
+            }
+            int add_length = 0;
+
+            if (length != 0)
+            {
+                add_length = length - parts[INT_PART].Count - parts[FRAC_PART].Count;
+                if (add_length < 0)
+                {
+                    parts[INT_PART].Clear();
+                    for(int i = 0; i < (length - precision); i++)
+                    parts[INT_PART].Add(9); // забиваем девяточками при превышении размера
+                    add_length = 0;
+                }
+            }
+            while (add_length-- != 0)
+            {
+                data.Add(0);
+            }
+
+            foreach (var part in parts)
+            {
+                foreach (var num in part)
+                {
+                    data.Add(num);
+                }
+            }
+
+
+
+        }
+
+        public void write_to(byte raw_data)
+        {
+            byte byte_data = raw_data;
+
+            /*
+            var it = data.begin();
+
+            if (has_sing_flag)
+            {
+                *byte_data = (sign == -1 ? 0 : 0x10) | *it;
+                byte_data++;
+                it++;
+            }
+            while (it != data.end())
+            {
+                uint8_t ddMMyyyy_presentation = *it << 4;
+                if (++it != data.end())
+                {
+                    ddMMyyyy_presentation |= *it;
+                    it++;
+                }
+                *byte_data = ddMMyyyy_presentation;
+                byte_data++;
+            }
+            */
+
+        }
+
+        public virtual String get_presentation()
+        {
+            String result = "";
+            if (has_sing_flag)
+            {
+                if (sign == -1)
+                {
+                    result += "-";
+                }
+            }
+            int int_size = data.Count - precision;
+            {
+                int i = 0;
+                while (i < int_size && data[i] == 0)
+                {
+                    i++;
+                }
+                if (i < int_size)
+                {
+                    while (i < int_size)
+                    {
+                        result += '0' + data[i];
+                        i++;
+                    }
+                }
+                else
+                {
+                    result += '0';
+                }
+            }
+            if (precision != 0)
+            {
+                String frac = ".";
+                bool has_significant_digits = false;
+                int max_significant_size = data.Count;
+                while (max_significant_size > int_size)
+                {
+                    if (data[max_significant_size - 1] == 0)
+                    {
+                        max_significant_size--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                for (int i = int_size; i < max_significant_size; i++)
+                {
+                    if (data[i] != 0)
+                    {
+                        has_significant_digits = true;
+                    }
+                    frac += '0' + data[i];
+                }
+                if (has_significant_digits)
+                {
+                    result += frac;
+                }
+            }
+            return result;
+        }
+
+        public String get_part(int startIndex, int count)
+        {
+            String result = "";
+            for (int i = startIndex; i < data.Count && count != 0; count--, i++)
+            {
+                result += (data[i] + '0');
+            }
+            return result;
+        }
+
+        public List<int> get_int()
+        {
+            List<int> result;
+
+            result = data.GetRange(0, data.Count - precision);
+
+            return result;
+            
+        }
+
+        public List<int> get_frac()
+        {
+            List<int> result;
+
+            result = data.GetRange(data.Count - precision, data.Count);
+            
+            return result;
+        }
 
 
         public bool has_sing_flag = false;
@@ -30,17 +269,86 @@ namespace _1STool1CD
 
     public class BinaryDecimalDate : BinaryDecimalNumber {
 
-        public BinaryDecimalDate(byte[] raw_data) { }
-        public BinaryDecimalDate(String presentation, String format = "dd.MM.yyyy hh:mm:ss") { }
+        public BinaryDecimalDate(byte raw_data) : base(raw_data, 19, 0, false)
+        {
+        }
 
-        public override String get_presentation() { return " "; }
+        public BinaryDecimalDate(String presentation, String format = "dd.MM.yyyy hh:mm:ss")
+        {
+            SortedDictionary<Char, List<int>> indexes = new SortedDictionary<char, List<int>>();
 
-        public int get_year() { return 0; }
-        public int get_month() { return 0; }
-        public int get_day() { return 0; }
+            for (int i = 0; i < format.Length; i++)
+            {
+                indexes.Add(format[i], new List<int>());
+            }
 
-        public int get_hour() { return 0; }
-        public int get_minute() { return 0; }
-        public int get_second() { return 0; }
+            for (int i = 0; i < format.Length; i++)
+            {
+                indexes[format[i]].Add(i);
+            }
+
+            foreach (var part in "yMdhms")
+            {
+                foreach (var i in indexes[part])
+                {
+                    if (i < presentation.Length)
+                    {
+                        data.Add(presentation[i] - '0');
+                    }
+                    else
+                    {
+                        data.Add(0);
+                    }
+                }
+            }
+
+        }
+
+        public override String get_presentation()
+        {
+            String result = "";
+            result += get_part(6, 2);
+            result += ".";
+            result += get_part(4, 2);
+            result += ".";
+            result += get_part(0, 4);
+            result += " ";
+            result += get_part(8, 2);
+            result += ":";
+            result += get_part(10, 2);
+            result += ":";
+            result += get_part(12, 2);
+            return result;
+        }
+
+        public int get_year()
+        {
+            return data[0] * 1000 + data[1] * 100 + data[2] * 10 + data[3]; 
+        }
+
+        public int get_month()
+        {
+            return data[4] * 10 + data[5];
+        }
+
+        public int get_day()
+        {
+            return data[6] * 10 + data[7];
+        }
+
+        public int get_hour()
+        {
+            return data[8] * 10 + data[9];
+        }
+
+        public int get_minute()
+        {
+            return data[10] * 10 + data[11];
+        }
+
+        public int get_second()
+        {
+            return data[12] * 10 + data[13];
+        }
     }
 }
---
+++ 1STool1CD/Common.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1STool1CD
+{
+    public static class Common
+    {
+        public static void time1CD_to_FileTime(DateTime ft, Char[] time1CD)
+        { }
+
+        public static UInt32 reverse_byte_order(UInt32 value)
+        {
+            return 0;
+        }
+
+        public static String GUIDas1C(Char[] fr)
+        {
+            return "";
+        }
+
+        public static String GUIDasMS(Char[] fr)
+        {
+            return "";
+        }
+
+        public static String GUID_to_string(Guid guid)
+        {
+            return "";
+        }
+
+        public static bool string_to_GUID(String str, Guid guid)
+        {
+            return true;
+        }
+
+        public static String GUID_to_string_flat(Guid guid)
+        {
+            return "";
+        }
+
+        public static bool string_to_GUID_flat(String str, Guid guid)
+        {
+            return true;
+        }
+
+        public static bool two_hex_digits_to_byte(Char hi, Char lo, Char[] res)
+        {
+            return true;
+        }
+
+        public static bool string1C_to_date(String str, DateTime bytedate)
+        {
+            return true;
+        }
+
+        public static bool string_to_date(String str, DateTime bytedate)
+        {
+            return true;
+        }
+
+        public static String date_to_string1C(DateTime bytedate)
+        {
+            return "";
+        }
+
+        public static String date_to_string(DateTime bytedate)
+        {
+            return "";
+        }
+
+        public static String hexstring(Char[] buf, int n)
+        {
+            return "";
+        }
+
+        public static String hexstring(Stream str)
+        {
+            return "";
+        }
+
+        public static String toXML(String in_str)
+        {
+            return "";
+        }
+
+        public static Char from_hex_digit(char digit)
+        {
+            return '0';
+        }
+
+
+    }
+}
