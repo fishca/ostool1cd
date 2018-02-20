a0f078b78fc0e2b1567c2ca4f3190feb005ce182
fishca <sergrud@mail.ru>
2018-02-20 23:12:18 +0300

    Этап доработки класса Field

--- 1STool1CD/FieldType.cs
+++ 1STool1CD/FieldType.cs
@@ -14,7 +14,7 @@ namespace _1STool1CD
         public Int32 precision;
         public bool case_sensitive;
 
-        public static field_type_declaration parse_tree(Tree field_tree) { return new field_type_declaration(); }
+        public static field_type_declaration parse_tree(tree field_tree) { return new field_type_declaration(); }
     }
 
     public class FieldType
@@ -28,11 +28,11 @@ namespace _1STool1CD
 
         public virtual String get_presentation(char[] rec, bool EmptyNull, char Delimiter, bool ignore_showGUID, bool detailed) { return " "; }
 
-        public virtual bool get_binary_value(char[] buf, String value) { return true; }
+        public virtual bool get_binary_value(byte[] buf, String value) { return true; }
 
-        public virtual String get_XML_presentation(char[] rec, Table parent, bool ignore_showGUID) { return " "; }
+        public virtual String get_XML_presentation(byte[] rec, Table parent, bool ignore_showGUID) { return " "; }
 
-        public virtual UInt32 getSortKey(char[] rec, char[] SortKey, Int32 maxlen) { return 0; }
+        public virtual UInt32 getSortKey(byte[] rec, byte[] SortKey, Int32 maxlen) { return 0; }
 
 
         public static FieldType create_type_manager(field_type_declaration type_declaration) { return (FieldType)null; }
